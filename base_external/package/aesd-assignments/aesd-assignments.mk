04fb6264eccec85cd5644791774d7337b55d934c
Marko Hammersen <marko.hammersen@gmail.com>
2025-11-20 13:03:39 +0100

    assignment 5 buildroot complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5f8634e53c21e7cf0167b20627872e42c993ba13
+AESD_ASSIGNMENTS_VERSION = 4a2aebacfa647a8a408fdf5ca7504d8b6539e1e6
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
